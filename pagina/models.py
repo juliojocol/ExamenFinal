b8f55c439106d557af8e5f8e3b6fe18c1a8e9721
Julio Jocol <juliojocol12@mesoamericana.edu.gt>
2021-12-02 00:36:21 -0600

    Creacion del modelo para base de datos

--- pagina/models.py
+++ pagina/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class Alumno(models.Model):
+    nombre  =   models.CharField(max_length=30)
+    fecha_nacimiento = models.DateField()
+
+    def __str__(self):
+        return self.nombre
+
+class Materia(models.Model):
+    nombre    = models.CharField(max_length=60)
+    anio      = models.IntegerField()
+    actores   = models.ManyToManyField(Alumno, through='Cursos')
+
+    def __str__(self):
+        return self.nombre
+
+class Cursos(models.Model):
+    actor = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    pelicula = models.ForeignKey(Materia, on_delete=models.CASCADE)
+
+class CursosInLine(admin.TabularInline):
+    model = Cursos
+    extra = 1
+
+
+class AlumnoAdmin(admin.ModelAdmin):
+    inlines = (CursosInLine,)
+
+class MateriaAdmin (admin.ModelAdmin):
+    inlines = (CursosInLine,)
