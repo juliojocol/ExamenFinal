740fa4079356a2ac34694d6ab68986a5dd602802
Julio Jocol <juliojocol12@mesoamericana.edu.gt>
2021-12-02 00:52:24 -0600

    crear vista de curso nuevo

--- pagina/views.py
+++ pagina/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import MateriaForm
+from pagina.models import Cursos, Materia
 
-# Create your views here.
+def curso_nueva(request):
+    if request.method == "POST":
+        formulario = MateriaForm(request.POST)
+        if formulario.is_valid():
+            curso = Materia.objects.create(nombre=formulario.cleaned_data['nombre'], anio = formulario.cleaned_data['anio'])
+            for actor_id in request.POST.getlist('actores'):
+                actuacion = Cursos(actor_id=actor_id, curso_id = curso.id)
+                actuacion.save()
+            messages.add_message(request, messages.SUCCESS, 'Curso Guardado Exitosamente')
+    else:
+        formulario = MateriaForm()
+    return render(request, 'pagina/curso_nuevo.html', {'formulario': formulario})
